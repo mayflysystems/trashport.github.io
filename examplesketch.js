364435d751d87cc89b73d890bee163a23730fa05
Nat <nat@optmax.com>
2021-10-19 16:42:49 -0700

    final draft

--- examplesketch.js
+++ examplesketch.js
@@ -19,6 +19,9 @@ let arrivedimg;
 // let arrowimg;
 let newlistimg;
 
+var x =329;
+var y =310;
+
 var backX = 350;
 var backY=30;
 
@@ -34,6 +37,8 @@ let selectedpage = false;
 let drivingpage = false;
 let arrivedpage = false;
 
+
+var counter = 10;
 function preload(){
   // img = loadImage('shrekophone.jpg');
   titleimg = loadImage('title.png');
@@ -55,7 +60,7 @@ function preload(){
 function setup() {
   createCanvas(428,926);
   // createCanvas(600,600);
-
+  frameRate(5);
 }
 function draw() {
   // x=x+changex;
@@ -66,46 +71,69 @@ function draw() {
     image(titleimg, 0, 0);
 
   }
-
-  // if (filledpage==true){
-  //   // square(exitX,exitY,20);
-  //   image(filledimg,0,0);
-  //
-  //   // square(exitX,exitY,20);
-  //   }
-  if (overview3==true){
+  if (addresspage==true){
     // square(exitX,exitY,20);
-    // image(haywardimg,0,0);
+    image(addressimg,0,0);
+
     // square(exitX,exitY,20);
     }
-  // if (overview4==true){
-  //     // square(exitX,exitY,20);
-  //     image(unionimg,0,0);
-  //     // square(exitX,exitY,20);
-  //   }
-  // if (overview5==true){
-  //       // square(exitX,exitY,20);
-  //       image(newarkimg,0,0);
-  //       // square(exitX,exitY,20);
-  //     }
-  // if (newpage==true){
-  //
-  //     image(newlistimg,0,0);
-  //
-  //
-  // }
-  // if (overview==true){
-  //   image(haywardimg,0,0);
-  // }
-//   if(x>300 || x<0){
-//  changex=changex*-1;
-// }
-// if(y<0 || y>300){
-//   changey=changey*-1
-// }
-  // noLoop();
+    if (filledpage==true){
+      // square(exitX,exitY,20);
+      image(filledimg,0,0);
 
-// rect(355,804,100,100);
+      // square(exitX,exitY,20);
+      }
+      if (listpage==true){
+        // square(exitX,exitY,20);
+        image(listimg,0,0);
+
+        // square(exitX,exitY,20);
+        }
+        if(selectedpage==true){
+          // square(exitX,exitY,20);
+
+          image(selectedimg,0,0);
+          stroke(255,0,0);
+          strokeWeight(4);
+          noFill();
+          ellipse(x,y,25,24);
+          if(counter==10){
+            // stroke(0,0,0);
+            // strokeWeight(4);
+            // noFill();
+            // ellipse(329,310,25,24);
+            x=1000;
+            y=1000;
+            counter=counter-10;
+          }
+          else if(counter==0){
+            counter = counter+10;
+            x=329;
+            y=310;
+          }
+
+
+
+          // square(exitX,exitY,20);
+          }
+          if (drivingpage==true){
+            // square(exitX,exitY,20);
+            image(drivingimg,0,0);
+
+            // square(exitX,exitY,20);
+            }
+            if (arrivedpage==true){
+              // square(exitX,exitY,20);
+              image(arrivedimg,0,0);
+
+              // square(exitX,exitY,20);
+              }
+
+  //
+  // if (overview3==false){
+  //
+  //   }
+
 }
 
 function mousePressed(){
@@ -113,68 +141,62 @@ function mousePressed(){
   if (mouseY>0 && mouseY<800 && titlepage==true){
     titlepage = false;
     addresspage = true;
-    if (addresspage==true){
-      // square(exitX,exitY,20);
-      image(addressimg,0,0);
 
-      // square(exitX,exitY,20);
-      }
 
   }
   if (mouseY>415 && mouseY<480 && mouseX>50 && mouseX<350 && addresspage==true){
     addresspage = false;
     filledpage = true;
-    if (filledpage==true){
-      // square(exitX,exitY,20);
-      image(filledimg,0,0);
 
-      // square(exitX,exitY,20);
-      }
 
   }
   if (mouseY>795 && mouseY<830 && mouseX>50 && mouseX<350 && filledpage==true){
     filledpage = false;
     listpage = true;
-    if (listpage==true){
-      // square(exitX,exitY,20);
-      image(listimg,0,0);
 
-      // square(exitX,exitY,20);
-      }
 
   }
   if (mouseY>480 && mouseY<580 && mouseX>50 && mouseX<350 && listpage==true){
+
     listpage = false;
     selectedpage = true;
-    if (selectedpage==true){
-      // square(exitX,exitY,20);
-      image(selectedimg,0,0);
 
-      // square(exitX,exitY,20);
-      }
+
+
       // rect(255,680,140,50);
   }
   if (mouseY>680 && mouseY<730 && mouseX>255 && mouseX<395 && selectedpage==true){
     selectedpage = false;
     drivingpage = true;
-    if (drivingpage==true){
-      // square(exitX,exitY,20);
-      image(drivingimg,0,0);
 
-      // square(exitX,exitY,20);
-      }
 
   }
   if (mouseY>0 && mouseY<300 && drivingpage==true){
     drivingpage = false;
     arrivedpage = true;
-    if (arrivedpage==true){
-      // square(exitX,exitY,20);
-      image(arrivedimg,0,0);
 
-      // square(exitX,exitY,20);
-      }
 
   }
+  if(listpage == true && mouseY>0 && mouseY<50 & mouseX>0 && mouseX<50){
+
+    addresspage = true;
+
+    filledpage = false;
+    listpage = false;
+    selectedpage = false;
+    drivingpage = false;
+    arrivedpage = false;
+  }
+  if(selectedpage == true && mouseY>0 && mouseY<50 & mouseX>0 && mouseX<50){
+
+    addresspage = true;
+
+    filledpage = false;
+    listpage = false;
+    selectedpage = false;
+    drivingpage = false;
+    arrivedpage = false;
+  }
+
 
 }
